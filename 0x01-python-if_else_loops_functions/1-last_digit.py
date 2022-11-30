25f939a0ee7e10d9761ff48de730be03d893c469
OnajiteChristabel <111015651+OnajiteChristabel@users.noreply.github.com>
2022-11-30 16:59:20 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 if number > 0:
     digit = abs(number) % 10
-elif number < 0:
+else:
     digit = number % -10
 if digit > 5:
     print("Last digit of {} is {} and is greater than 5"
